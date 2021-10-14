23a287f469ec9677c9ccd01b866ccab88aa81576
chry902 <tchry902@gmail.com>
2021-10-14 15:24:58 +0200

    inizio esercizio

---
+++ assets/main.js
@@ -0,0 +1,25 @@
+
+//chiedi all'utente la sua email
+/*prompt*/
+
+
+//controlla che l'email sia nella listadi chi puo accedere
+
+
+//stampa un messaggio appropriatosull'esito del controllo
+
+
+
+
+
+
+
+
+
+
+//gernera un numero random da 1 a 6 sia per il giocatore che per il coputer
+/*Math.floor(Math.random() * 6);*/
+
+
+//stabilisci il vincitore in base al punteggio più alto
+/*if */
