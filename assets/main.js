2114243be15611cab4f739535c163cf3f18cf317
chry902 <tchry902@gmail.com>
2021-10-14 17:02:23 +0200

    inserimento nomi sui numeri creati comp/gioca....

--- assets/main.js
+++ assets/main.js
@@ -5,9 +5,9 @@
 
 
 const utente = Math.floor(Math.random() * 6);
-console.log(utente);
+console.log(utente, "giocatore");
 const computer = Math.floor(Math.random() * 6);
-console.log(computer);
+console.log(computer, "computer");
 //gernera un numero random da 1 a 6 sia per il giocatore che per il coputer
 /**/
 
